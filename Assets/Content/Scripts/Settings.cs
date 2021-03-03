9addba777d27b3b432f749817b811ccf9e5eea11
Anton Savranskiy <47571615+sm011a@users.noreply.github.com>
2021-03-03 17:12:36 +0300

    Весь проект

---
+++ Assets/Content/Scripts/Settings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Settings : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
